33cc2785c239bf7f2e1aeabf542fa6ed2dc22e64
VidurK2 <54384846+VidurK2@users.noreply.github.com>
2021-12-18 22:12:44 +0530

    Palindrome - Vidur

---
+++ LeetCode/Palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        
+        if x == x[::-1]:
+            return True
+        else:
+            return False

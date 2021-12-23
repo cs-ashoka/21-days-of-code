9aa0675d9e31bfd17e02d762601d17f6c62d5b91
Argha Chakrabarty <ac380012@gmail.com>
2021-12-24 04:01:13 +0530

    added files

---
+++ Day 06/MergeSortedArray_argha-dot.py
@@ -0,0 +1,9 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(len(nums2)):
+            nums1[m + i] = nums2[i]
+        nums1 = nums1.sort()
+        
